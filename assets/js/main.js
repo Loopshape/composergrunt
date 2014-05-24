194b170cafec1162bbb4d703fe61b32f2e079d9f
Arjuna Noorsanto <webmaster@loopshape.com>
2014-05-24 23:36:17 +0200

    Update main.js
    updated dependencies

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 // load bootstrap
-console.log( '\n\nLOOPSHAPE presents:\n\GRUNT+COMPOSER SKELETON\n' );
-console.log( 'Contact: webmaster@loopshape.com\n\n' );
-console.log( 'Starting App!\n' );
+console.log('\n\nLOOPSHAPE presents:\n\GRUNT+COMPOSER SKELETON\n');
+console.log('Contact: webmaster@loopshape.com\n\n');
+console.log('Starting App!\n');
 // start the RJS config
-require.config( {
-	
-	baseUrl : '/assets/js',
+require.config({
+
+	baseUrl : './assets/js/',
 	// enter correct pathname for /server/frame
-	
+
 	paths : {
 		// Require.JS/BACKBONE CoreFiles
 		jquery : './app/core/jquery-2.0.3.min',
@@ -19,32 +19,21 @@ require.config( {
 		tweenmax : './app/lib/GreenSock/TweenMax.min',
 		timelinemax : './app/lib/GreenSock/TimelineMax.min',
 		sticky : './app/lib/garand-sticky-c199302/jquery.sticky',
-		syntaxhighlighter : './app/lib/syntaxhighlighter_3.0.83/scripts/shCore',
-		shautoloader : './app/lib/syntaxhighlighter_3.0.83/scripts/shAutoloader',
-		shapplescript : './app/lib/syntaxhighlighter_3.0.83/scripts/shBrushAppleScript',
-		shbrushjs : './app/lib/syntaxhighlighter_3.0.83/scripts/shBrushJScript',
 		iframeautoheight : './app/lib/jquery-autoheight/jquery.iframe-auto-height.plugin.1.9.3.min',
 		tooltipsy : './app/lib/tooltipsy-master/tooltipsy.min',
-		spritely : './app/lib/spritely-0.6.8/src/jquery.spritely',
 		pace : './app/lib/pace.min',
 		// Website Logic
 		app : './app/app',
-		template : './app/class/_template',
-		clocktimer : './app/class/_clocktimer',
-		canvas : './app/class/_canvas',
-		scwidget : './app/lib/soundcloud-custom-player-master/js/soundcloud.player.api',
-		scplayer : './app/lib/soundcloud-custom-player-master/js/sc-player',
-		eqheights : './app/lib/jQuery.equalHeights-master/jquery.equalheights.min',
-		benchmark : './app/lib/benchmark.js-master/benchmark'
+		template : './app/class/_template'
 	},
-	
+
 	shim : {
 		jquery : {
 			exports : '$'
 		},
 		migrate : {
 			deps : ['jquery']
-		}, 
+		},
 		underscore : {
 			deps : ['jquery'],
 			exports : '_'
@@ -63,28 +52,9 @@ require.config( {
 			deps : ['backbone'],
 			exports : 'Template'
 		},
-		clocktimer : {
-			deps : ['backbone'],
-			exports : 'ClockTimer'
-		},
 		sticky : {
 			deps : ['jquery'],
 			exports : 'Sticky'
-		},
-		syntaxhighlighter : {
-			exports : 'SyntaxHighlighter'
-		},
-		shautoloader : {
-			deps : ['syntaxhighlighter'],
-			exports : 'autoloader'
-		},
-		shbrushjs : {
-			deps : ['syntaxhighlighter'],
-			exports : 'brushes'
-		},
-		shapplescript : {
-			deps : ['syntaxhighlighter'],
-			exports : 'shapplescript'
 		},
 		iframeautoheight : {
 			deps : ['jquery'],
@@ -94,32 +64,16 @@ require.config( {
 			deps : ['jquery'],
 			exports : 'Tooltipsy'
 		},
-		spritely : {
-			deps : ['jquery'],
-			exports : 'Spritely'
-		},
-		scwidget : {
-			deps : ['jquery'],
-			exports : 'SC'
-		},
-		scplayer : {
-			deps : ['scwidget'],
-			exports : 'SCPlayer'
-		},
-		eqheights : {
-			deps : ['jquery'],
-			exports : 'EqualHeights'
-		},
 		pace : {
 			deps : ['jquery'],
 			exports : 'Pace'
 		},
 		app : {
-			deps : ['migrate', 'template', 'clocktimer', 'canvas', 'tooltipsy', 'iframeautoheight', 'spritely', 'scwidget', 'scplayer', 'eqheights', 'pace'],
+			deps : ['jquery', 'migrate', 'template', 'pace'],
 			exports : 'App'
 		}
 	},
-	
-	waitSeconds: 7
-	
-} ), define(["jquery", "underscore", "backbone", 'tweenmax', 'timelinemax', "app"]);
+
+	waitSeconds : 0
+
+}), define(["jquery", "underscore", "backbone", 'tweenmax', 'timelinemax', "app"]); 
